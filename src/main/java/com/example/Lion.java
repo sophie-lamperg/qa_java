e53245432accc68072abee691114410f203ca130
Sophie <vasilevna-sofiya@mail.ru>
2022-12-13 18:36:24 +0400

    add 1

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.List;
 
 public class Lion {
-
+    private IFeline feline;
     boolean hasMane;
 
     public Lion(String sex) throws Exception {
@@ -16,7 +16,9 @@ public class Lion {
         }
     }
 
-    Feline feline = new Feline();
+    public Lion (IFeline feline) {
+        this.feline = feline;
+    }
 
     public int getKittens() {
         return feline.getKittens();
